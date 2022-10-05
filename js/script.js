e8108ed3020a3e35826d405946e5605361a8b418
JamesCLB <jamesdanielxavier@gmail.com>
2022-10-04 22:40:13 -0300

    Adicionei Eventos

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function inserirProduto() { 
+    var nomeProduto = document.getElementById("nomeProduto") 
+    var produtos = document.getElementById("produtos")  
+    var liProduto = document.createElement("li")
+    var checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox") 
+    checkbox.addEventListener("click", function() {
+        window.prompt("Adicione o valor da compra")
+    })
+    var spanProduto = document.createElement("span")
+    spanProduto.innerHTML = nomeProduto.value 
+    var btnExcluir = document.createElement("input")
+    btnExcluir.setAttribute("type", "button")
+    btnExcluir.setAttribute("value", "X")
+    btnExcluir.addEventListener("click", function () {
+        liProduto.remove()
+    })
+    produtos.append(liProduto)
+    liProduto.append(checkbox, spanProduto, btnExcluir)      
+}
