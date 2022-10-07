89f6aa43be9e86e7e3ae8ccae0e85141b463e047
JamesCLB <114442136+JamesCLB@users.noreply.github.com>
2022-10-07 17:48:46 -0300

    Atualização multiplicador
    Adicionei o botão de colocar quantos (do mesmo produto) você quer tipo
    
    5 batata = batata x 5

--- js/script.js
+++ js/script.js
@@ -6,36 +6,46 @@ function inserirProduto() {
     var produtos = document.getElementById("produtos")
     var liProduto = document.createElement("li")
     var checkbox = document.createElement("input")
+    var btnVezes = document.createElement("input")
+    btnVezes.setAttribute("type", "number")
     checkbox.setAttribute("type", "checkbox")
     checkbox.addEventListener("click", function () {
-        if (checkbox.checked) { 
+        if (checkbox.checked) {
             valorDoproduto = parseFloat(prompt("Adicione o valor da compra"))
             if (!valorDoproduto) {
                 valorDoproduto = 0
                 checkbox.checked = false
                 return
             }
+            btnVezesValor = btnVezes.value
+            valorDoproduto = valorDoproduto * btnVezesValor
             valorDascomprasTotal.innerHTML = parseFloat(valorDascomprasTotal.innerHTML) + valorDoproduto
-
+            dinheiroDoPobre()
         } else {
+            btnVezesValor = btnVezes.value
+            valorDoproduto = valorDoproduto 
             valorDascomprasTotal.innerHTML = parseFloat(valorDascomprasTotal.innerHTML) - valorDoproduto
             valorDoproduto = 0
-
+            dinheiroDoPobre()
         }
-     })
+    })
     var spanProduto = document.createElement("span")
     spanProduto.innerHTML = nomeProduto.value
     var btnExcluir = document.createElement("input")
     btnExcluir.setAttribute("type", "button")
     btnExcluir.setAttribute("value", "X")
-    btnExcluir.setAttribute("img", "X")
     btnExcluir.addEventListener("click", function () {
         liProduto.remove()
+        btnVezesValor = btnVezes.value
+        console.log(btnVezesValor)
+        valorDoproduto = valorDoproduto 
         valorDascomprasTotal.innerHTML = parseFloat(valorDascomprasTotal.innerHTML) - valorDoproduto
+        dinheiroDoPobre()
 
     })
     produtos.append(liProduto)
-    liProduto.append(checkbox, spanProduto, btnExcluir)
+    liProduto.append(checkbox, spanProduto, btnExcluir, btnVezes)
+
 }
 function dinheiroDoPobre() {
     var dinheiroInicial = document.getElementById("dinheiroInicial")
@@ -48,10 +58,7 @@ function dinheiroDoPobre() {
 }
 document.addEventListener("keypress", function (e) {
     if (e.key === "Enter") {
-        window.prompt("gay")
-        bnt = document.getElementById("ok")
-        btn.click()
+        dinheiroDoPobre()
+
     }
 })
-
-
